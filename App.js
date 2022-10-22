6af230de8080ce05161726683cd2b08462f227c5
SrijananiSiva <111624988+SrijananiSiva@users.noreply.github.com>
2022-10-22 20:08:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios  from 'axios';
+
+function App() {
+  const getNews =() =>{
+    Axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=da5e11e4f5c14c1aac34f67d00097d8c&q=india")
+    .then((response)=>{
+      console.log(response)
+    })
+  }
+  return (
+    <>
+    <div>
+      <button className='App' onClick={getNews}>GET NEWS</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
